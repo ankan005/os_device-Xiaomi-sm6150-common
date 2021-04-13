44e0b31f237cd3796c57ebb7bf5777507bbb2660
Ankan005 <Ankan005@gmail.com>
2021-04-13 06:55:00 +0000

    broken rule

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -103,6 +103,7 @@ BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 swiotlb=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += kpti=off
+BUILD_BROKEN_DUP_RULES := true
 
 # TARGET_KERNEL_APPEND_DTB handling
 ifeq ($(strip $(PRODUCT_USE_DYNAMIC_PARTITIONS)),true)
